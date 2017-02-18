e82ff8e44fa36200e0e5fc5d8ce424081398da70
Lehyu <951908719@qq.com>
2017-02-18 07:08:57 -0800

    4

---
+++ doing/4/findMedianSortedArrays.cpp
@@ -0,0 +1,33 @@
+/*
+  There are two sorted arrays nums1 and nums2 of size m and n respectively.
+  Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+  Example 1:
+  nums1 = [1, 3]
+  nums2 = [2]
+  The median is 2.0
+*/
+
+class Solution {
+public:
+  double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+      vector<int> nums;
+      int mid;
+      int i,j;
+      for(i = 0, j = 0; !nums2.empty()&&!nums1.empty()&&i < nums1.size()&& j < nums2.size();){
+	bool flag = nums1[i] >= nums2[j];
+	flag?nums.push_back(nums1[i]):nums.push_back(nums2[j]);
+	flag?i++:j++;
+      }
+      bool flag = i < nums1.size() || nums2.empty();
+      int k = 0;
+      vector<int> tmp;
+      flag?tmp = nums1: tmp = nums2;
+      flag?k = i: k = j;
+      while(k < tmp.size()){
+	nums.push_back(tmp[k]);
+	k++;
+      }
+      mid = nums.size()/2;
+      return nums.size()%2 != 0?nums[mid]:(nums[mid]+nums[mid+1])/2.0; // double
+    }
+};
